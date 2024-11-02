e070d4b72cf3c6cac04a619ad5fe2b06bae322be
Pandiyaraj S. <UT01117@unicomtic.com>
2024-11-02 18:58:12 +0530

    updated'

--- src/app/list-task/list-task.component.ts
+++ src/app/list-task/list-task.component.ts
@@ -12,20 +12,20 @@ export class ListTaskComponent implements OnInit {
 
   searchText: string = '';
 
- tasks: Task[] = [];
- 
-constructor(  private router: Router , private taskService : TaskserviceService, private route: ActivatedRoute) {
-        
+  tasks: Task[] = [];
+
+  constructor(private router: Router, private taskService: TaskserviceService, private route: ActivatedRoute) {
+
 
   }
 
- ngOnInit(): void {
-   this.loadTask()
- 
- }
+  ngOnInit(): void {
+    this.loadTask()
+
+  }
 
   onDelete(taskId: number) {
-    if(confirm('Do you want to delete?')) {
+    if (confirm('Do you want to delete?')) {
       this.taskService.DeleteTask(taskId).subscribe(data => {
         // this.toastr.success('Task is deleted', "Deleted", {
         //   timeOut: 10000,
@@ -38,8 +38,10 @@ constructor(  private router: Router , private taskService : TaskserviceService,
 
   loadTask() {
     this.taskService.getTask().subscribe(d => {
+      console.log(d);
+      
       this.tasks = d;
-      console.log(this.tasks);
+
     })
   }
 
--- src/app/task-filter.pipe.ts
+++ src/app/task-filter.pipe.ts
@@ -17,3 +17,4 @@ export class TaskFilterPipe implements PipeTransform {
 
 }
 
+
